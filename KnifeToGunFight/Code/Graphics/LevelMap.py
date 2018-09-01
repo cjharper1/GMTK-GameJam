3347266a1855deaedbce149d6afde552fdfa603b
Hannah M <hannah.e.mcneill@gmail.com>
2018-09-01 13:42:32 -0400

    Create level map and game object classes

---
+++ KnifeToGunFight/Code/Graphics/LevelMap.py
@@ -0,0 +1,46 @@
+# Class for displaying the map of a level.
+from KnifeToGunFight.Code.Objects import GameObject
+
+
+class LevelMap(object):
+    def __init__(self, currentLevel):
+        # Define the path to the current level.
+        self.LevelMapFilepath = f'Maps/Level{currentLevel}.txt'
+
+        # The height and width of the map.
+        self.MapWidth = 0
+        self.MapHeight = 0
+
+        # A list of game objects that make up the map.
+        self.Map = []
+
+        # Build the map.
+
+    # Opens up the file for the map of the given level and builds the game objects and map.
+    def ParseMap(self):
+        # READ THE MAP FILE.
+        with open(self.LevelMapFilepath, 'r') as map_file:
+            map_rows = map_file.readlines()
+
+        # Remove newline characters from the rows.
+        map_rows = [row.strip() for row in map_rows]
+
+        # DETERMINE THE HEIGHT AND WIDTH OF THE MAP.
+        self.MapHeight = len(map_rows)
+        self.MapWidth = len(max(map_rows, key = len))
+
+        # Loop through the file and create game objects for the map.
+        current_pixel_row_position = 0
+        current_pixel_column_position = 0
+        for row_index, row in enumerate(map_rows):
+            # Calculate the starting pixesl position for objects in the current row.
+            y_position = (row_index * GameObject.HeightPixels)
+
+            # Parse the current row into game objects.
+            for column_index, ascii_object in enumerate(list(row)):
+                # Calculate starting X coordinate.
+                x_position = (column_index * GameObject.WidthPixels)
+
+                # Create the GameObject and add it to the map
+                object = GameObject(y_position, x_position, row_index, column_index)
+                self.Map.append(object)
---
+++ KnifeToGunFight/Code/Objects/GameObject.py
@@ -0,0 +1,36 @@
+import pygame
+
+
+# Base class of game objects that will be displayed on the map
+class GameObject(object):
+    # The width of a a game object in pixels.
+    WidthPixels = 32
+    # The height of a game object in pixels.
+    HeightPixels = 32
+
+    @property
+    def TopLeftCornerPosition(self):
+        return self.Coordinates.topleft
+
+    @property
+    def TopRightCornerPosition(self):
+        return self.Coordinates.topright
+
+    @property
+    def BottomLeftCornerPosition(self):
+        return self.Coordinates.bottomleft
+
+    @property
+    def BottomRightCornerPosition(self):
+        return self.Coordinates.bottomright
+
+    def __init__(self, initial_top_left_x_position, initial_top_left_y_position, row_index, column_index):
+        # Create a rectangle object to store the coordinates.
+        self.Coordinates = pygame.Rect(
+            initial_top_left_x_position,
+            initial_top_left_y_position,
+            self.WidthPixels,
+            self.HeightPixels)
+
+        self.RowIndex = row_index
+        self.ColumnIndex = column_index
